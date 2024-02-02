b3fe486218c4246e933e627211803f5c1e4060ec
Nyakio-Eva <evalkariuki@gmail.com>
2024-02-02 18:29:07 +0300

    completed assignment

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 function Container({
-  header,
-  children,
-  textPosition = "", // possible values: left, right, center
-  direction = "horizontal", // possible values: vertical, horizontal
-  contentPosition = "center", // possible values: left, right, center
+  header, //represents a header or title for the container, if a header is provided, it renders an h2 element with the specified header text(example header)
+  children, //represents the content that will be placed inside the container, the {children} placeholder will be placed with the actual content passed to the component
+  textPosition = "", // possible values: left, right, center. controls the horizontal alignment of text, adds a css class to the container based on the specified text position
+  direction = "horizontal", // possible values: vertical, horizontal. defines the layout direction of the container and its children, adds a css class to the inner based on a specified direction
+  contentPosition = "center", // possible values: left, right, center. controls the horizontal alignment of the content within the inner container,adds a css class to the inner container based on the specified content position
 }) {
   return (
     <div className={`container ${textPosition}`}>
